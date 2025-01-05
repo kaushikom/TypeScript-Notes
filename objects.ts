482c13f828cbbbba29849a12fed429c159c8b149
Om Kaushik <omkaushik015@gmail.com>
2025-01-05 12:45:49 +0530

    add notes on type aliases, readonly and optional

--- objects.ts
+++ objects.ts
@@ -34,3 +34,73 @@ const fact = {answer:true, reason:'We saw his grave', dog:'alive'}
 
 // Allowed
 isJohnDead(fact)
+
+
+// Type aliases
+
+type User = {
+    name: string,
+    email: string,
+    isActive: boolean
+}
+
+function checkUser(user: User){}
+
+// Allowed
+checkUser(user)
+
+const anotherUser = {
+    name: "John Wick",
+    isActive: false
+}
+
+// Not allowed
+// checkUser(anotherUser)
+// Hence we can use the type aliases to fix the problem with object types
+
+// This type can be used for any new user objects
+
+const newUser: User = {
+    name:"Winston",
+    email:"winston@continental.com",
+    isActive:true
+}
+
+// Readonly and optional
+type Dog = {
+    name : string,
+    readonly breed : string,
+    hasOwner?: boolean // '?' marks the property as optional
+}
+
+const dogNextDoor: Dog = {
+    name : "Santa",
+    breed: 'husky',
+    hasOwner: true
+}
+
+// Allowed
+dogNextDoor.name = 'Popeye'
+
+// Not allowed
+// dogNextDoor.breed = 'husky'
+
+// Using two or more types in one
+
+type cardNumber = {
+    cardNumber : number
+}
+
+type cardDate = {
+    expiryDate : string
+}
+
+type cardDetails = cardNumber & cardDate & {
+    cvv : number
+}
+
+const myCard: cardDetails = {
+    cardNumber: 1234,
+    expiryDate: "Jan 2025",
+    cvv: 321
+}
